0cd0b6c326e2112d2470882e6817370ce0e9193d
Alejandro Gil <ajgp8821@gmail.com>
2023-05-19 18:17:29 -0300

    Toggle Drawer - Show Hide Menu

--- App.tsx
+++ App.tsx
@@ -1,20 +1,43 @@
 import 'react-native-gesture-handler'
-import React from 'react'
+import React, { useEffect } from 'react'
 import { StatusBar } from 'expo-status-bar'
 import { StyleSheet, Text, View } from 'react-native'
-import { NavigationContainer } from '@react-navigation/native'
+import { NavigationContainer, DefaultTheme } from '@react-navigation/native'
+import * as ScreenOrientation from 'expo-screen-orientation'
 import { StackNavigator } from './src/navigator/StackNavigator'
 import { BasicSideMenu } from './src/navigator/BasicSideMenu'
+import { style } from './src/theme/appTheme'
 
 export default function App() {
+	// console.log(DefaultTheme)
+
+	useEffect(() => {
+		// Enable rotation
+		ScreenOrientation.unlockAsync()
+	}, [])
+  
 	return (
 		// <View style={styles.container}>
-		<NavigationContainer>
+		<NavigationContainer
+			// theme={MyTheme}
+		>
 			{/* <StackNavigator /> */}
 			<BasicSideMenu />
 			{/* <StatusBar style="auto" /> */}
 		</NavigationContainer>
 	)
+}
+
+const MyTheme = {
+	dark: false,
+	colors: {
+		primary: 'rgb(255, 45, 85)',
+		background: 'rgb(242, 242, 242)',
+		card: 'rgb(200, 200, 200)',
+		text: 'rgb(28, 28, 30)',
+		border: 'rgb(199, 199, 204)',
+		notification: 'rgb(255, 69, 58)',
+	},
 }
 
 // const styles = StyleSheet.create({
--- src/navigator/BasicSideMenu.tsx
+++ src/navigator/BasicSideMenu.tsx
@@ -2,14 +2,30 @@ import React from 'react'
 import { createDrawerNavigator } from '@react-navigation/drawer'
 import { StackNavigator } from './StackNavigator'
 import { SettingsScreen } from '../screens/SettingsScreen'
+import { Dimensions, ScaledSize, useWindowDimensions } from 'react-native'
 
 const Drawer = createDrawerNavigator()
 
+// const { width: ww, height: wh }: ScaledSize = Dimensions.get('window')
+// const { width: sw, height: sh } = Dimensions.get('screen')
+
 export const BasicSideMenu = () => {
+
+	const { width/*, height: dh*/ } = useWindowDimensions()
+
+	// const dType = ww >= 768 ? 'permanent' : 'front'
+	// console.log('dType', dType)
+  
 	return (
-		<Drawer.Navigator>
-			<Drawer.Screen name="StackNavigator" component={StackNavigator} />
-			<Drawer.Screen name="SettingsScreen" component={SettingsScreen} />
+		<Drawer.Navigator
+			screenOptions={{
+				// drawerPosition:'right'
+				drawerType: width >= 768 ? 'permanent' : 'front', // Horizontal mode menu
+				// headerShown: false  // Hide the hamburger icon
+			}}
+		>
+			<Drawer.Screen name="StackNavigator" options={{title:'Home'}} component={StackNavigator} />
+			<Drawer.Screen name="SettingsScreen" options={{title:'Settings'}} component={SettingsScreen} />
 		</Drawer.Navigator>
 	)
 }
