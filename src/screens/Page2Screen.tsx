95e886bb284194e9c27dc8d9107c4b34201a3513
Alejandro Gil <ajgp8821@gmail.com>
2023-05-18 22:04:34 -0300

    Stacks

--- src/screens/Page2Screen.tsx
+++ src/screens/Page2Screen.tsx
@@ -1,19 +1,33 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React, { useEffect } from 'react'
+import { Button, Text, View } from 'react-native'
+import { useNavigation } from '@react-navigation/native'
+import { StackNavigationOptions } from '@react-navigation/stack'
+import { style } from '../theme/appTheme'
 
 export const Page2Screen = () => {
+
+	const navigator = useNavigation()
+
+	const options: StackNavigationOptions = {
+		title: 'Page 2',
+		headerBackTitle: 'Go Back'
+	}
+
+	useEffect(() => {
+		navigator.setOptions(options)
+	}, [])
+  
+
 	return (
-		<View style={styles.container}>
-			<Text>
+		<View style={style.globalMargin}>
+			<Text style={style.title}>
         Page2Screen
 			</Text>
+
+			<Button
+				title='Go Pag 3'
+				onPress={() => navigator.navigate('Page3Screen')}
+			/>
 		</View>
 	)
 }
-
-const styles = StyleSheet.create({
-	container: {
-		// flex: 1,
-		height: '100%',
-	}
-})
